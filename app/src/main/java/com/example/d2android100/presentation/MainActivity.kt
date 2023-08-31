a468c4f8d1e530c26797562a9e4e8561f37e569f
IslombekYadgarov <islam67747@gmail.com>
2023-08-31 11:18:13 +0500

    Initial commit

---
+++ app/src/main/java/com/example/d2android100/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.d2android100.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.d2android100.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
